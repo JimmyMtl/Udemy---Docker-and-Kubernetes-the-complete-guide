198e2aa2fd1e1165ea6bcd65b661757f94734fb0
Jimmy MARTEL <jimmy.martel@razorfish.fr>
2022-12-13 00:02:30 +0000

    167

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,32 @@ import Fib from "./common/pages/Fib";
 import {BrowserRouter as Router, Route, Link, Routes} from "react-router-dom";
 import logo from './logo.svg'
 import OtherPage from "./common/pages/OtherPage";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 function App() {
     return (
-        <Router>
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className={"App-logo"} alt="logo"/>
-                    <h1 className="App-title">Welcome to React</h1>
-                    <div className="App-container-link">
-                        <Link className={"App-link"} to={"/"}>Home</Link>
-                        <Link className={"App-link"} to={"/otherpage"}>Other Page</Link>
-                    </div>
-                </header>
-                <Routes>
-                    <Route exact path={"/"} element={<Fib/>}/>
-                    <Route path={"/otherpage"} element={<OtherPage/>}/>
-                </Routes>
-            </div>
-        </Router>
+        <QueryClientProvider client={queryClient}>
+            <Router>
+                <div className="App">
+                    <header className="App-header">
+                        <div className={"App-title-logo-wrapper"}>
+                            <img src={logo} className={"App-logo"} alt="logo"/>
+                            <h1 className="App-title">Welcome to React</h1>
+                        </div>
+                        <div className="App-container-link">
+                            <Link className={"App-link"} to={"/"}>Home</Link>
+                            <Link className={"App-link"} to={"/otherpage"}>Other Page</Link>
+                        </div>
+                    </header>
+                    <Routes>
+                        <Route exact path={"/"} element={<Fib/>}/>
+                        <Route path={"/otherpage"} element={<OtherPage/>}/>
+                    </Routes>
+                </div>
+            </Router>
+        </QueryClientProvider>
     );
 }
 
--- client/src/common/pages/Fib.js
+++ client/src/common/pages/Fib.js
@@ -1,83 +1,128 @@
-import React, { useEffect, useState } from "react";
+import React, {useState} from "react";
 import axios from "axios";
+import {useQuery, useMutation, useQueryClient} from '@tanstack/react-query'
+import Spinner from "../components/Spinner";
 
 const Fib = () => {
-  const [state, setState] = useState({
-    seenIndexes: [],
-    values: {},
-    index: "",
-  });
-  const fetchValues = async () => {
-    const values = await axios.get("/api/values/current");
-    setState((precState) => ({ ...precState, values: values.data }));
-  };
-
-  const fetchIndexes = async () => {
-    const seenIndexes = await axios.get("/api/values/all");
-    setState((precState) => ({
-      ...precState,
-      seenIndexes: seenIndexes.data,
-    }));
-  };
-  useEffect(() => {
-    fetchValues();
-    fetchIndexes();
-  }, []);
-
-  const handleSubmit = async (event) => {
-    // event.preventDefault();
-
-    await axios.post("/api/values", {
-      index: state.index,
-    });
-    setState((precState) => ({ ...precState, index: "" }));
-  };
-
-  const renderSeenIndexes = () => {
-    return state.seenIndexes.map(({ number }) => number).join(", ");
-  };
-
-  const renderValues = () => {
-    const entries = [];
-
-    for (let key in state.values) {
-      entries.push(
-        <div key={key}>
-          For index {key} I calculated {state.values[key]}
-        </div>
-      );
-    }
-
-    return entries;
-  };
-
-  return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="">Enter your index:</label>
-          <input
-            type="text"
-            value={state.index}
-            onChange={(event) =>
-              setState((precState) => ({
-                ...precState,
-                index: event.target.value,
-              }))
+    const queryClient = useQueryClient({
+        defaultOptions: {
+            queries: {
+                retry: 1,
+                staleTime: 5,
             }
-          />
-          <button type={"submit"}>Submit</button>
+        }
+    });
+    const [state, setState] = useState({
+        index: "",
+    });
+    const fetchValues = async () => {
+        const res = await axios.get("/api/values/current");
+        console.log('values')
+        return res.data
+    };
+
+    const fetchIndexes = async () => {
+        const seenIndexes = await axios.get("/api/values/all");
+        console.log('fetchIdnexes')
+        return seenIndexes.data
+    };
+
+    const {data: indexes, status: indexesStatus, isLoading: loadingIndexes} = useQuery(['indexes'], fetchIndexes, {
+        onSuccess: (dat) => {
+            console.log('fetchDat ', dat)
+        }
+    });
+    const {data: values, status: valuesStatus, isLoading: loadingValues} = useQuery(['values'], fetchValues);
+
+    const postValues = async (index) => {
+        const res = await axios.post("/api/values", {
+            index,
+        });
+        return res.data
+
+    }
+    const {mutate, isLoading: submitLoading} = useMutation(postValues, {
+        onSuccess: (data) => {
+            console.log('success ???', data)
+        },
+        onError: (error) => {
+            console.log("there was an error", error)
+        },
+        onSettled: () => {
+            queryClient.invalidateQueries('values');
+        }
+    })
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        mutate(state.index)
+    };
+
+    const renderSeenIndexes = () => {
+        return indexes?.map(({number}) => (<tr key={number}>
+            <th>{number}</th>
+        </tr>));
+    };
+
+    const renderValues = () => {
+        const entries = [];
+
+        for (let key in values) {
+            entries.push(
+                <tr key={key}>
+                    <th>{key}</th>
+                    <th>{values[key]}</th>
+                </tr>
+            );
+        }
+
+        return entries;
+    };
+    if (submitLoading || loadingIndexes || loadingValues) {
+        return <Spinner/>
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="">Enter your index:</label>
+                    <input
+                        type="number"
+                        value={state.index}
+                        onChange={(event) =>
+                            setState((precState) => ({
+                                ...precState,
+                                index: event.target.value,
+                            }))
+                        }
+                    />
+                    <button type={"submit"}>Submit</button>
+                </div>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Seen indexes</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {renderSeenIndexes()}
+                    </tbody>
+                </table>
+                <h3>Calculated values:</h3>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Index</th>
+                        <th>Calculated</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {renderValues()}
+                    </tbody>
+                </table>
+            </form>
         </div>
-        <h3>Indicies I have seen:</h3>
-        {renderSeenIndexes()}
-        <h3>Calculated values:</h3>
-        {renderValues()}
-        {/*<div>*/}
-        {/*    I calculated {JSON.stringify(state.values)}*/}
-        {/*</div>*/}
-      </form>
-    </div>
-  );
+    );
 };
 
 export default Fib;
